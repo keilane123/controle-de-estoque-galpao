156b80ba886a83e81c2364f46ca4d892df8c57f0
keilane123 <keilanesoaresfarias@gmail.com>
2026-07-25 16:13:34 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'controle-estoque-v2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Estratégia: tenta a rede primeiro (para pegar dados atualizados);
+// se falhar (sem internet), usa o que estiver salvo em cache.
+self.addEventListener('fetch', (event) => {
+  if(event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
